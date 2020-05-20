9dce6fef7dd1a9bbc58bf4beca6768cebf6e6dd9
Bondoc Marcian <marcy_whitewolf2010@yahoo.com>
2020-05-20 18:10:06 +0300

    LIB-25 Design the general look

---
+++ src/main/java/controllers/mainWindowController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.text.Text;
+
+
+
+import java.io.IOException;
+
+
+public class mainWindowController {
+    @FXML
+    private TextField PasswordL;
+    @FXML
+    private TextField UserNameL;
+    @FXML
+    private Text loginMessage;
+
+    public void handleRegister(ActionEvent actionEvent) throws IOException {
+
+    }
+
+    public void handleLogin(ActionEvent actionEvent) {
+
+    }
+}
